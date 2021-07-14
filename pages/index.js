3c9ba2d2eca2190746720635a6245a35f7833fbd
Suriya <922780.local@03HW049195.local>
2021-07-14 14:42:48 +0100

    Homepage

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,18 @@
 import Link from "next/link";
-
-import Header from "../components/Header";
+import Image from 'next/image'
+import profilePic from '../public/images/summer-mw.jpg'
+import styles from "../styles/Home.module.css";
 
 const Index = () => (
-  <Header>
-    <br />
-    <Link href="/plp">
-      <a> PLP</a>
-    </Link>
-  </Header>
-  
+  <div className={styles.description}> Get Personal 
+ <div className={styles.homepage}>
+    <p>Go to your profile <Link href="/shirts" className="button">Suriya</Link></p>
+   <p>Go to your site <Link href="/dresses" className="button">Suriya</Link></p>  
+    <p>Our collection of men’s tops has all the styles you need. Our polos come in a range of colours and stripes, whether you love knitted styles or classic long-sleeved versions. Our tops come in classic cotton for everyday wear and are perfect for casual downtime. Choose moisture-wicking fabric for jogging or working out at home</p>
+    <Image src={profilePic} alt="Picture of the author" width="400" height="400"/>
+ 
+ </div>
+ </div>
 );
 
 export default Index;
